1724dd47528b2c476dcc7b6a1619012757f0159f
Lionel Baker <lionelbakerza@gmail.com>
2022-09-14 21:59:36 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Lionel Baker on 2022/09/14.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
